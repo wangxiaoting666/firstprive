ff17587a8aa417fd31a2232f069c9e8ff743433c
wangting <1357097537@qq.com>
2018-12-21 13:51:58 +0800

    ting

---
+++ pages/deploy/deploy.js
@@ -0,0 +1,103 @@
+// pages/leftSwiperDel/index.js
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+
+    this.tempData();
+  },
+  // 自定义弹框
+  deployed: function () {
+    wx.navigateTo({
+      url: '../deploy/deploy'
+      //  url: '../logs/logs'
+    })
+  },
+
+  //测试临时数据
+  tempData: function () {
+    var list = [
+      {
+        rank: "intel酷睿5000",
+        txtStyle: "",
+        icon: "/images/details/CPU.png",
+        name: "CPU",
+        pace: "¥151",
+
+
+      },
+      {
+        rank: "intel酷睿5000",
+        txtStyle: "",
+        icon: "/images/details/zhuban.png",
+        name: "主板",
+        pace: "¥151",
+
+      },
+      {
+        rank: "intel酷睿5000",
+        txtStyle: "",
+        icon: "/images/details/neicun.png",
+        name: "内存",
+        pace: "¥151",
+
+      },
+      {
+        rank: "intel酷睿5000",
+        txtStyle: "",
+        icon: "/images/details/ssd.png",
+        name: "SSD",
+        pace: "¥151",
+
+      },
+      {
+        rank: "intel酷睿5000",
+        txtStyle: "",
+        icon: "/images/details/xianka.png",
+        name: "显卡",
+        pace: "¥151",
+
+      },
+      {
+        rank: "intel酷睿5000",
+        txtStyle: "",
+        icon: "/images/details/jixiang.png",
+        name: "机箱",
+        pace: "¥151",
+
+      },
+      {
+        rank: "intel酷睿5000",
+        txtStyle: "",
+        icon: "/images/details/dianyuan.png",
+        name: "电源",
+        pace: "¥151",
+
+      },
+      {
+        rank: "intel酷睿5000",
+        txtStyle: "",
+        icon: "/images/details/xianshiqi.png",
+        name: "显示器",
+        pace: "¥151",
+      },
+      {
+        rank: "intel酷睿5000",
+        txtStyle: "",
+        icon: "/images/details/jianpan.png",
+        name: "键鼠套",
+        pace: "¥151",
+
+      },]
+
+
+
+    //
+    this.setData({
+      list: list
+    });
+  }
+
+})
